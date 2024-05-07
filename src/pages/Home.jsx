563f4cd52390c1dc4319aee09e38a9792691257b
Glya-Corporation <alfonsouzcategui1811@gmail.com>
2024-05-07 15:51:07 -0500

    update Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <main>
+      <article>
+        <section>
+          <img src="" alt="photo"/>
+        </section>
+      </article>
+    </main>
+  )
+}
+
+export default Home;
