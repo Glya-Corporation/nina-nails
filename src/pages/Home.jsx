9d76d9c14ab5ad513ec714fa427166a35218c421
Luis <alfonsouzcategui@gmail.com>
2024-05-07 21:48:50 -0500

    gota

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,14 @@
 import { useState } from 'react';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.jpg';
+import img6 from '../assets/6.jpg';
+import img7 from '../assets/7.jpg';
+import img8 from '../assets/8.jpg';
+import img9 from '../assets/9.jpg';
+import img10 from '../assets/10.jpg';
 
 const Home = () => {
   const [colorSelected, setColorSelected] = useState('');
@@ -8,16 +18,21 @@ const Home = () => {
     { id: 2, color: '#ff0000' },
     { id: 3, color: '#0000ff' },
     { id: 4, color: '#ffffff' },
-    { id: 4, color: '#000000' },
-    { id: 4, color: '#008800' }
+    { id: 5, color: '#000000' },
+    { id: 6, color: '#008800' }
   ];
 
   const listNails = [
-    { id: 1, img: '' },
-    { id: 2, img: '' },
-    { id: 3, img: '' },
-    { id: 4, img: '' },
-    { id: 4, img: '' }
+    { id: 1, img: img1 },
+    { id: 2, img: img2 },
+    { id: 3, img: img3 },
+    { id: 4, img: img4 },
+    { id: 5, img: img5 },
+    { id: 6, img: img6 },
+    { id: 7, img: img7 },
+    { id: 8, img: img8 },
+    { id: 9, img: img9 },
+    { id: 10, img: img10 }
   ];
 
   return (
@@ -44,7 +59,7 @@ const Home = () => {
         <section className='color-section'>
           <ul className='list-colors' style={{ borderBottom: `1px solid ${colorSelected}`, boxShadow: `0 10px 10px${colorSelected}50` }}>
             {listColors.map(item => (
-              <li key={item.id} style={{ backgroundColor: item.color }} className='item-color' onClick={() => setColorSelected(item.color)}></li>
+              <li key={item.id} style={{ backgroundColor: item.color }} className='item-color' onClick={() => setColorSelected(item.color)}><span style={{backgroundColor: `${item.color}`}}></span></li>
             ))}
           </ul>
         </section>
@@ -52,7 +67,7 @@ const Home = () => {
           <ul className='list-nails'>
             {listNails.map(item => (
               <li key={item.id} className='item-nail'>
-                {item.id}
+                <img src={item.img} alt="img" />
               </li>
             ))}
           </ul>
