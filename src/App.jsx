a4ef95c547b2c8f88942e09bfa67abc8db67b309
Glya-Corporation <alfonsouzcategui1811@gmail.com>
2024-05-14 08:48:55 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import Soon from './component/Soon'
 import Home from './pages/Home';
 
 function App() {
   return (
     <HashRouter>
+    <Soon />
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
---
+++ src/component/Soon.jsx
@@ -0,0 +1,14 @@
+const Sonn = () => {
+  return (
+    <main className='soon'>
+      <h2>
+        Sitio Web
+      </h2>
+      <h1>
+        ¡próximamente disponible!
+      </h1>
+    </main>
+  )
+}
+
+export default Soon;
