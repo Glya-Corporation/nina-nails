73c5b22c0a8d4dbd9823071cfc106e6a649d639d
Luis <alfonsouzcategui@gmail.com>
2024-05-17 11:45:21 -0500

    url

--- src/component/Soon.jsx
+++ src/component/Soon.jsx
@@ -1,15 +1,17 @@
+import wsicon from '../assets/wsicon.png';
+
 const Soon = () => {
+  const openUrl = () => window.open('https://wa.me/message/654FJEPM5II2J1?src=qr');
+
   return (
     <main className='soon'>
       <h2>
-        <p>
-          Sitio Web
-        </p>
+        <p>Sitio Web</p>
         ¡próximamente disponible!
       </h2>
-      
+      <img className='wsicon' src={wsicon} alt='' onClick={() => openUrl()} />
     </main>
-  )
-}
+  );
+};
 
 export default Soon;
